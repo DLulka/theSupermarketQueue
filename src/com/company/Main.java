2554c29c5ef38892d6f6ee040affdc298dc15af2
daniel.lulka <daniel.lulka@bbva.com>
2019-10-07 09:46:56 -0500

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,30 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println(Main.solveSuperMarketQueue(new int[] { 4,4,6,5,1,1,3,1,6,5,5,2,1,7,2,4,5,7,5,7 }, 4));
+	System.out.println(Main.solveSuperMarketQueue(new int[] { 3,3,3,4,1,2,7,3,6,6,5,6,4,4,4,3,1,1 }, 5));
     }
     public static int solveSuperMarketQueue(int[] customers, int n) {
         int registers[] = new int[n];
-        int low = 0, high = 0;
+        int low = 0, high = 0, test = 0;
         for(int i = 0; i < customers.length; i++){
             for(int k = 0; k < registers.length; k++){
-                System.out.println("Registers low tiene: " + registers[low] + " y registers k tiene: " + registers[k] );
                 if(registers[low] <= registers[k]){
                 }else {
                     low = k;
                 }
-                System.out.println("Low tiene: " + low);
             }
             registers[low] = registers[low] + customers[i];
         }
-        if(registers.length == 1){
+        high = registers[0];
+        if(customers.length == 1 || registers.length == 1){
             high = registers[0];
         }else {
             for (int i = 0; i < registers.length; i++) {
                 if ((i + 1) == registers.length) {
                 } else {
-                    if (registers[i] > registers[i + 1]) {
-                        high = registers[i];
+                    if (high > registers[i + 1]) {
                     } else {
                         high = registers[i + 1];
                     }
